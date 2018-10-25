180bd9912218fe7dad8f51d12c823272d4a55a09
Justin D. Fetherolf <fetherolfjd@gmail.com>
2018-10-24 20:12:00 -0600

    Handle portrait and landscape layouts.
    This handle redrawing the app to a degree
    for the different orientations of the device.

--- lib/src/clock_cluster.dart
+++ lib/src/clock_cluster.dart
@@ -4,27 +4,43 @@ import 'clock.dart';
 import 'alarm_time_page.dart';
 import 'hour_minute_text.dart';
 import 'bloc_provider.dart';
+import 'alarm_bloc.dart';
 
 class ClockCluster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final alarmBloc = BlocProvider.of(context).alarmBloc;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Clock(),
-        IconButton(
-          icon: Icon(Icons.add_alarm, color: Colors.white),
-          tooltip: 'Set alarm',
-          onPressed: () => Navigator.of(context).pushNamed(AlarmTimePage.routeName)
-        ),
-        StreamBuilder<String>(
-          stream: alarmBloc.alarmTime,
-          builder: (context, snapshot) {
-            return ColorText(snapshot.data, Colors.white);
-          }
-        ),
-      ],
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if (orientation == Orientation.landscape) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _buildWidgetList(alarmBloc, context),
+          );
+        } else {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _buildWidgetList(alarmBloc, context)
+          );
+        }
+      },
     );
+  }
+
+  List<Widget> _buildWidgetList(AlarmBloc alarmBloc, BuildContext context) {
+    return <Widget>[
+      Clock(),
+      IconButton(
+        icon: Icon(Icons.add_alarm, color: Colors.white),
+        tooltip: 'Set alarm',
+        onPressed: () => Navigator.of(context).pushNamed(AlarmTimePage.routeName)
+      ),
+      StreamBuilder<String>(
+        stream: alarmBloc.alarmTime,
+        builder: (context, snapshot) {
+          return ColorText(snapshot.data, Colors.white);
+        }
+      ),
+    ];
   }
 }
--- lib/src/red_green_homepage.dart
+++ lib/src/red_green_homepage.dart
@@ -1,24 +1,60 @@
 import 'package:flutter/material.dart';
 
-import 'red_green_lights.dart';
-import 'bloc_provider.dart';
 import 'clock_cluster.dart';
+import 'lights.dart';
 
 class RedGreenHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final alarmBloc = BlocProvider.of(context).alarmBloc;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ClockCluster(),
-            RedGreenLights(),
-          ],
-        )
-      )
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          final width = MediaQuery.of(context).size.width;
+          final height = MediaQuery.of(context).size.height;
+          if (orientation == Orientation.landscape) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClockCluster(),
+                _buildRedLight(orientation, width, height),
+                _buildGreenLight(orientation, width, height),
+              ]
+            );
+          } else {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClockCluster(),
+                _buildRedLight(orientation, width, height),
+                _buildGreenLight(orientation, width, height),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildRedLight(Orientation orientation, double width, double height) {
+    return _buildLight(orientation, width, height, RedLight());
+  }
+
+  Widget _buildGreenLight(Orientation orientation, double width, double height) {
+    return _buildLight(orientation, width, height, GreenLight());
+  }
+
+  Widget _buildLight(Orientation orientation, double width, double height, Widget childWidget) {
+    double largeDimension = width > height ? width : height;
+    double totalWidgetSpace = largeDimension * 0.4;
+    double widgetSpace = totalWidgetSpace * 0.9;
+    double widgetMargin = totalWidgetSpace * 0.1;
+    EdgeInsetsGeometry edgeInset = orientation == Orientation.portrait ? EdgeInsets.only(top: widgetMargin) : EdgeInsets.only(left: widgetMargin);
+    return Container(
+      height: widgetSpace,
+      width: widgetSpace,
+      margin: edgeInset,
+      child: childWidget
     );
   }
 }
